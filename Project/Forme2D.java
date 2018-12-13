2fc79c82e88a647fba2c0077757120fd30bb8a37
Lucas Emilio <lucasemilio.mf@gmail.com>
2018-12-13 18:15:48 +0100

    Update Forme2D.java

--- Project/Forme2D.java
+++ Project/Forme2D.java
@@ -1,68 +1,86 @@
-import java.awt.Color;
+package Exercice;
 
-public class Forme2D {
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class Forme2D {
 	private Point2D P;
 	private Color C;
+	private int nombreFormes = 0;
 	
 	// Constructors
-	Forme2D(){
+	public Forme2D(){
 		this.P = new Point2D();
-		this.C = new Color(0,0,255);
+		setColor(java.awt.Color.blue);
+		nombreFormes++;
 	}
-	Forme2D(Point2D P){
+	public Forme2D(Point2D P){
 		try {
 			if(P.getX() < 0 || P.getY() < 0) throw new WrongOriginException();
 			else this.P = P;
 		}
 		catch(WrongOriginException woe) {
 			System.err.println(woe.getMessage());
+			if(P.getX() < 0) P.setX(-P.getX());
+			if(P.getY() < 0) P.setY(-P.getY());
 		}
-		this.C = new Color(0,0,255);
+		setColor(java.awt.Color.blue);
+		nombreFormes++;
 	}
-	Forme2D(Color C){
+	public Forme2D(Color C){
 		this.P = new Point2D();
-		this.C= C;
+		setColor(C);
+		nombreFormes++;
 	}
-	Forme2D(Point2D P, Color C){
+	public Forme2D(Point2D P, Color C){
 		try {
 			if(P.getX() < 0 || P.getY() < 0) throw new WrongOriginException();
 			else this.P = P;
 		}
 		catch(WrongOriginException woe) {
 			System.err.println(woe.getMessage());
+			if(P.getX() < 0) P.setX(-P.getX());
+			if(P.getY() < 0) P.setY(-P.getY());
 		}
-		this.C = C;
+		setColor(C);
+		nombreFormes++;
 	}
-	
 	
 	// Setter and getters
-	void setColor(int r, int g, int b) {
+	public void setColor(int r, int g, int b) {
 		this.C = new Color(r,g,b);
 	}
-	void setColor(Color C) {
+	public void setColor(Color C) {
 		this.C = C;
 	}
-	Color getColor() {
+	void setPoint(int x, int y) {
+		P.setX(x);
+		P.setY(y);
+	}
+	public Color getColor() {
 		return this.C;
 	}
-	Point2D getOrigin() {
+	public Point2D getOrigin() {
 		return this.P;
 	}
-	double getX() {
+	public int getX() {
 		return this.P.getX();
 	}
-	double getY() {
+	public int getY() {
 		return this.P.getY();
+	}
+	public int getNombreFormes() {
+		return nombreFormes;
 	}
 	
 	// 
-	void translation(Point2D P) {
+	public void translation(Point2D P) {
 		this.P = P;
 	}
 	public String toString() {
 		return P.toString()+':'+getColor();
 	}
-	double surface() {
-		return 0;
-	}
+	public abstract double surface();
+	public abstract void dessine(Graphics g);
+	
 }
