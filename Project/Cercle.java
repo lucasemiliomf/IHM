8dd0c28da94e2a96eef2468d85754f49be08d331
Lucas Emilio <lucasemilio.mf@gmail.com>
2018-12-13 18:16:39 +0100

    Update Cercle.java

--- Project/Cercle.java
+++ Project/Cercle.java
@@ -1,18 +1,30 @@
-import java.awt.*;
+package Exercice;
+
+import java.awt.Graphics;
 
 public class Cercle extends Forme2D{
-	private double rayon;
-	Cercle(){
+	private int rayon;
+	public Cercle(){
 		rayon = 0;
 	}
-	Cercle(double rayon){
-		this.rayon= rayon;
+	public Cercle(int rayon){
+		this.rayon = rayon;
+	}
+	public Cercle(int rayon, Point2D P){
+		this.rayon = rayon;
+		setPoint(P.getX(),P.getY());
 	}
 	
-	double surface() {
+	@Override
+	public double surface() {
 		return rayon*rayon*Math.PI;
 	}
-
+	@Override
+	public void dessine(Graphics g) {
+	    g.setColor(getColor());
+		g.drawOval(getX(), getX(), 2*rayon, 2*rayon);
+	}
+	@Override
 	public String toString() {
 		return "Cercle:"+super.toString()+":"+rayon+":"+surface();
 	}
