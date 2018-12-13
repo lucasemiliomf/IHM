079925afe3d39a567614fd29514f8a874fefeb02
Lucas Emilio <lucasemilio.mf@gmail.com>
2018-12-13 18:15:22 +0100

    Update Point2D.java

--- Project/Point2D.java
+++ Project/Point2D.java
@@ -1,21 +1,27 @@
+package Exercice;
 
 public class Point2D {
-	private double x, y;
+	private int x, y;
 	Point2D(){
 		setPoint(0,0);
 	}
 	Point2D(int x, int y){
 		setPoint(x,y);
 	}
-	
 	void setPoint(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	double getX() {
+	void setX(int x) {
+		this.x = x;
+	}
+	void setY(int y) {
+		this.y = y;
+	}
+	int getX() {
 		return x;
 	}
-	double getY() {
+	int getY() {
 		return y;
 	}
 	public String toString() {
