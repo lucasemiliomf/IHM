6d7bfba79dd6c9b6a84f010c514d4d9896e69005
Lucas Emilio <lucasemilio.mf@gmail.com>
2018-12-13 18:16:17 +0100

    Update Carre.java

--- Project/Carre.java
+++ Project/Carre.java
@@ -1,15 +1,36 @@
+package Exercice;
+
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Carre extends Forme2D{
-	private double cote;
+	private int cote;
 	
-	void setCote(double cote) {
+	public Carre(int cote){
+		setCote(cote);
+	}
+	public Carre(int cote, int x, int y){
+		setCote(cote);
+		setPoint(x,y);
+	}
+	public Carre(int cote, int x, int y, Color C){
+		setCote(cote);
+		setPoint(x,y);
+		setColor(C);
+	}
+	void setCote(int cote) {
 		this.cote = cote;
 	}
-	public double getCote() {
+	public int getCote() {
 		return cote;
 	}
-	double surface() {
+	@Override
+	public double surface() {
 		return cote*cote;
+	}
+	public void dessine(Graphics g) {
+	    g.setColor(getColor());
+		g.drawRect(getX(), getY(), cote, cote);
 	}
 	public String toString() {
 		return "Carre:"+super.toString()+":"+cote+":"+surface();
